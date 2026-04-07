321c3bb994c5da34f209b3e357046f128e715578
SCUTPangty <1179478742@qq.com>
2026-04-07 18:54:48 +0800

    algorithm try

--- Trees/BasicOperationofTrees.cpp
+++ Trees/BasicOperationofTrees.cpp
@@ -117,14 +117,32 @@ int FindMax(Node* root)
     return FindMax(root->right);
 }
 
-int FindHeight(Node *root)
+int FindHighestHeight(Node *root)
 {
     if(root==nullptr) return 0;
     int left = 0, right = 0;
-    left = FindHeight(root->left);
-    right = FindHeight(root->right);
+    left = FindHighestHeight(root->left);
+    right = FindHighestHeight(root->right);
 
     return max(left,right)+1;
+}
+
+int FindLowestHeight(Node *node)
+{
+    if (node == NULL) return 0;
+    int leftDepth = FindLowestHeight(node->left);           // 左
+    int rightDepth = FindLowestHeight(node->right);         // 右
+                                                    // 中
+    // 当一个左子树为空，右不为空，这时并不是最低点
+    if (node->left == NULL && node->right != NULL) { 
+        return 1 + rightDepth;
+    }   
+    // 当一个右子树为空，左不为空，这时并不是最低点
+    if (node->left != NULL && node->right == NULL) { 
+        return 1 + leftDepth;
+    }
+    int result = 1 + min(leftDepth, rightDepth);
+    return result;
 }
 
 Node *InvertTree(Node *root)
@@ -180,6 +198,106 @@ Node* lowestCommonAncestor(Node* root, Node* p, Node* q)
     if(left == nullptr) return right;//左边没有，右边有
     if(right == nullptr) return left;//右边没有，左边有
     return root;//两边各有一个
+}
+
+int findBottomLeftValue(Node* root) 
+{
+    queue<Node*>que;
+    if(root != nullptr) que.push(root);
+    int result = 0;
+
+    while(!que.empty())
+    {
+        int size = que.size();
+        for(int i=0; i< size ;i++)
+        {//记录每行的节点，画图理解一下
+            Node *node = que.front();
+            que.pop();//que只保存每行第一个值
+            //记录最后一行第一个元素
+            if(i == 0) result = node->data;
+            if(node->left) que.push(node->left);
+            if(node->right) que.push(node->right);
+        }
+    }
+    return result;//爱来自代码随想录
+}
+
+int sumOfLeftLeaves(Node* root) 
+{
+    if(root == nullptr) return 0;
+    if(root->left == nullptr && root->right == nullptr) return 0;
+
+    int leftValue = sumOfLeftLeaves(root->left);//左
+    if(root->left && !root->left->left && !root->left->right)
+    {
+        leftValue = root->left->data;
+    }
+    int rightValue = sumOfLeftLeaves(root->right);//右
+    //求的是左叶子节点的和，与右叶子节点无关，有左孩子自然会加上的
+
+    int sum = leftValue + rightValue;//中
+    return sum;//爱来自代码随想录
+}
+
+void traversal(Node *cur, vector<int>& path, vector<string>& result)
+{//爱来自代码随想录
+    path.push_back(cur->data);//中为什么写在这里，因为最后一个节点也要加入path中
+    // 这才到了叶子节点
+    if(cur->left == nullptr && cur->right == nullptr)//遇见叶子节点
+    {
+        string sPath;//各种操作，只为博AC一绿
+        for(int i=0; i<path.size()-1; i++)
+        {//path里的路径转为string
+            sPath += to_string(path[i]);
+            sPath += "->";
+        }//要写箭头，故loop里不能写最后一个节点
+        //记录最后一个节点(叶子)
+        sPath += to_string(path[path.size() -1]);
+        result.push_back(sPath);//收集整个路径
+        return;
+    } 
+    if(cur->left)
+    {//左
+        traversal(cur->left, path, result);
+        path.pop_back();//回溯
+    }
+    if(cur->right)
+    {//右
+        traversal(cur->right, path, result);
+        path.pop_back();//回溯
+    }
+}
+
+vector<string> binaryTreePaths(Node* root)
+{//爱来自代码随想录
+    vector<string> result;
+    vector<int> path;
+    if(root == nullptr) return result;
+    traversal(root, path, result);
+    return result;
+}
+
+int getBalancedHeight (Node *node)
+{
+    if(node == nullptr) return 0;
+
+    int leftHeight = getBalancedHeight(node->left);//左
+    if(leftHeight == -1) return -1;
+    int rightHeight = getBalancedHeight(node->right);//右
+    if(rightHeight == -1) return -1;
+    //中
+    int result;
+    if (abs(leftHeight - rightHeight) > 1) {  // 中
+        result = -1;
+    } else {
+        // 以当前节点为根节点的树的最大高度
+        result = 1 + max(leftHeight, rightHeight);
+    }
+    return result;
+}
+
+bool isBalanced(Node* root) {
+    return getBalancedHeight(root) == -1 ? false : true;
 }
 
 int main()
